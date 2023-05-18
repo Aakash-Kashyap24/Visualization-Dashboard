94b885c18327cd57d963a634a437cb53dfbca3fe
Akash <aakasgdev24@gmail.com>
2023-05-19 04:13:11 +0530

    Auth-logou-added-in-client

--- client/src/pages/Admin/Dashboard.jsx
+++ client/src/pages/Admin/Dashboard.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+// import { select, append } from 'd3';
+
 const Dashboard = () => {
   return (
     <div>
--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -3,18 +3,26 @@ import Header from "../../components/Header";
 import Login from "../../components/Login";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from "react-router-dom";
 import {useAlert} from 'react-alert'
+import { loadUser } from "../../action/userAction";
 
 
 
 export default function LoginPage() {
 
+    const dispatch = useDispatch()
     const alert=useAlert();
     const Navigate=useNavigate();
     const {user,isLoading,isAuthenticated,error}=useSelector((state)=>state.user)
-   
+  useEffect(() => {
+  
+    if (isAuthenticated) {
+        Navigate('/admin/dashoard')
+    }
+    
+  }, [isAuthenticated,error,alert])
 
     useEffect(() => {
         if (error) {
@@ -23,6 +31,7 @@ export default function LoginPage() {
         if (isAuthenticated) {
             Navigate('/admin/dashboard')
         }
+        dispatch(loadUser())
     }, [isAuthenticated])
 
     useEffect(() => {
